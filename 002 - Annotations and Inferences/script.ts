f19f7199c3d0543897fb9c94a6779f2e1cfcb017
Hugo Deiró <hugodeiro@gmail.com>
2016-11-01 21:43:22 -0300

    002 - Annotations

---
+++ 002 - Annotations and Inferences/script.ts
@@ -0,0 +1,18 @@
+//##########################
+// ANNOTATIONS 
+//##########################
+
+//Declaration (var), name (variable), annotation (:), type(number)
+var variable : number;
+
+/**
+ * Other possible types could be:
+ * 
+ * - number    -> var variable:number = 10;
+ * - string    -> var variable:string = 'test'
+ * - array     -> var variable:(type)[] = [1,2,3]; //Considering type as number
+ * - multitype -> var variable[(type1), (type2)] = [10, 'test']; // Considering type1 as number and type2 as string
+ * - any       -> var variable:any = 1; //Could be anything.
+ * - null      -> var variable:null;
+ * - undefined -> var variable:undefined;
+ */
